77ae27ac8317e83c5c984776652029be9b5c5856
Cam Sinclair <csinkers@gmail.com>
2024-04-30 07:57:04 +0000

    Add assembly name utility

---
+++ src/an/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace AssemblyNameUtil;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        if (args.Length != 1 || !File.Exists(args[0]))
+        {
+            Console.WriteLine("an - 'AssemblyNameUtil', this utility emits the .NET assembly name of the given file.");
+            Console.WriteLine("Usage: an.exe <dll or exe path>");
+            return 1;
+        }
+
+        try 
+        {
+            var name = AssemblyName.GetAssemblyName(args[0]);
+            Console.WriteLine(name.FullName);
+        }
+        catch (BadImageFormatException)
+        {
+            Console.WriteLine("File does not contain a .NET Assembly");
+            return 1;
+        }
+
+        return 0;
+    }
+}
+
---
+++ src/an/README.md
@@ -0,0 +1,2 @@
+# AN
+'Assembly Name', this program just emits the .NET assembly name of the given file.

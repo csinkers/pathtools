d0ec49232225c5f8749438180e2dcfafde8395d2
Cam Sinclair <csinkers@gmail.com>
2025-06-24 12:46:43 +1000

    Add bin/Debug/[runtime]/ support to CdBin

--- src/CdBin/Program.cs
+++ src/CdBin/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CdBin
 {
@@ -13,56 +13,59 @@ namespace CdBin
             Console.Error.WriteLine("Navigate to the most relevant bin directory beneath the current path for debug or release");
         }
 
-        static KeyValuePair<string, DateTime> LatestExe(KeyValuePair<string, DateTime> current, string path)
+        static IEnumerable<(string Path, DateTime LatestExeTime)> GetEntries(string path)
         {
+            foreach (var dir in Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly))
+            {
+                var name = Path.GetFileName(dir);
+                if (name.StartsWith(".") || name.Equals("obj", StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+
+                foreach (var entry in GetEntries(dir))
+                    yield return entry;
+            }
+
             var exes = Directory.GetFiles(path, "*.exe", SearchOption.TopDirectoryOnly);
             if (exes.Any())
             {
                 var time = exes.Max(x => File.GetLastWriteTimeUtc(x));
-                if (time > current.Value)
-                    return new KeyValuePair<string, DateTime>(path, time);
+                yield return (path, time);
             }
-
-            return current;
         }
 
         static int Main(string[] args)
         {
-            if(args.Length == 0)
+            if (args.Length == 0)
             {
                 Usage();
                 return 1;
             }
 
             string pattern = string.Empty;
-            switch(args[0].ToLower())
+            switch (args[0].ToLower())
             {
-                case "debug":
-                    pattern = "*debug*";
-                    break;
-                case "release":
-                    pattern = "*release*";
-                    break;
-                default: 
+                case "debug":   pattern = "*debug*";   break;
+                case "release": pattern = "*release*"; break;
+                default:
                     Usage();
                     return 1;
             }
+
             var curDir = Directory.GetCurrentDirectory();
             if (!Directory.Exists("bin"))
                 return 1;
 
             var dirs = Directory.GetDirectories("bin", pattern, SearchOption.AllDirectories);
-            var bestDir = dirs.Aggregate(new KeyValuePair<string, DateTime>(string.Empty, DateTime.MinValue), LatestExe, x => x.Key);
-            if (!string.IsNullOrEmpty(bestDir))
+            var entries = dirs.Select(x => GetEntries(x)).SelectMany(x => x).OrderByDescending(x => x.LatestExeTime);
+            foreach (var entry in entries)
             {
-                Console.WriteLine(new DirectoryInfo(bestDir).FullName);
+                Console.WriteLine(new DirectoryInfo(entry.Path).FullName);
                 return 0;
             }
-            else
-            {
-                Console.Error.WriteLine("No best directory found");
-                return 1;
-            }
+
+            Console.Error.WriteLine("No best directory found");
+            return 1;
         }
     }
 }
